283b522b4858305d90766bbff72a3c1d3249d98f
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-16 16:28:53 +0200

    [tsk-00003] Added the examples of working with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css';
 import GameField from "./component/GameField.jsx";
+import JsxPracticeComponent from "./jsx_practice/JsxPracticeComponent.jsx";
+import ChildrenComponent from "./jsx_practice/ChildrenComponent.jsx";
+import PracticeUseState from "./jsx_practice/PracticeUseState.jsx";
 
 function App() {
-    return <GameField/>;
+    return <>
+        <GameField/>
+        <JsxPracticeComponent type={'Input'} typeElement={'type_element'}/>
+        <JsxPracticeComponent typeElement={'several_elements'}/>
+        <ChildrenComponent><h1>h1 element</h1></ChildrenComponent>
+        <ChildrenComponent><h2>h2 element</h2></ChildrenComponent>
+        <PracticeUseState/>
+    </>;
 }
 
 export default App;
---
+++ src/jsx_practice/PracticeUseState.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+const PracticeUseState = () => {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => setCounter((lastCounter) => lastCounter < 4 ? lastCounter + 1 : lastCounter);
+
+    const incrementBy3 = () => {
+        increment();
+        increment();
+        increment();
+    }
+
+    const decrement = () => setCounter(counter - 1);
+
+
+    return <div>
+        <button onClick={() => increment()}>Increment</button>
+        <button onClick={() => incrementBy3()}>Increment by 3</button>
+        <button onClick={() => decrement()}>Decrement</button>
+        <h3>{counter}</h3>
+    </div>
+}
+
+export default PracticeUseState;
